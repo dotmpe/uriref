dc5048c308cf794532a88c562dfe658de1ea1dd7
B. van Berkum <dev@dotmpe.com>
2016-08-15 12:21:04 +0200

    Travis build deps

---
+++ install-dependencies.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+set -e
+
+test -z "$Build_Debug" || set -x
+
+test -z "$Build_Deps_Default_Paths" || {
+  test -n "$SRC_PREFIX" || SRC_PREFIX=$HOME/build
+  test -n "$PREFIX" || PREFIX=$HOME/.local
+}
+
+test -n "$sudo" || sudo=
+
+test -n "$SRC_PREFIX" || {
+  echo "Not sure where checkout"
+  exit 1
+}
+
+test -n "$PREFIX" || {
+  echo "Not sure where to install"
+  exit 1
+}
+
+test -d $SRC_PREFIX || ${sudo} mkdir -vp $SRC_PREFIX
+test -d $PREFIX || ${sudo} mkdir -vp $PREFIX
+
+
+install_docopt()
+{
+  test -n "$sudo" || install_f="--user"
+  git clone https://github.com/dotmpe/docopt-mpe.git $SRC_PREFIX/docopt-mpe
+  ( cd $SRC_PREFIX/docopt-mpe \
+      && git checkout 0.6.x \
+      && $sudo python ./setup.py install $install_f )
+}
+
+
+
+main_entry()
+{
+  test -n "$1" || set -- '-'
+
+  case "$1" in '-'|python|project|docopt)
+      # Using import seems more robust than scanning pip list
+      python -c 'import docopt' || { install_docopt || return $?; }
+    ;; esac
+
+  echo "OK. All pre-requisites for '$1' checked"
+}
+
+
+test "$(basename $0)" = "install-dependencies.sh" && {
+  while test -n "$1"
+  do
+    main_entry "$1" || exit $?
+    shift
+  done
+} || printf ""
+
+# Id: uriref/0 install-dependencies.sh
