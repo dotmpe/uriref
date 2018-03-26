d452e366f8cc2977991789e5cf7b36f5874872ab
B. van Berkum <dev@dotmpe.com>
2018-03-26 13:56:10 +0200

    Traceback for exampples

--- bin/examples.py
+++ bin/examples.py
@@ -1,3 +1,4 @@
+import traceback
 import uriref
 from uriref import util
 
@@ -23,12 +24,18 @@ if __name__ == '__main__':
         uris.insert(0, '/?root')
         uris.insert(0, '/root')
         uris.insert(0, 'urn:root')
+        # NOTE: ED2K urls use pipe characters and are not valid URI references.
+        #uris.insert(0, 'ed2k://.file.The_Two_Towers-The_Purist_Edit-Trailer.avi.14997504.965c013e991ee246d63d45ea71954c4d./')
+        # NOTE: magnet URI's do not have a path so their query part is not
+        # recognized within the boundaries for URI's set by RFX 2396. It syntax
+        # is fine, and a parser for magnet only needs to match that group
+        uris.insert(0, 'magnet:?xt=urn:ed2k:31D6CFE0D16AE931B73C59D7E0C089C0&xl=0&dn=zero_len.fil&xt=urn:bitprint:3I42H3S6NNFQ2MSVX7XZKYAYSCX5QBYJ.LWPNACQDBZRYXW3VHJVCJ64QBZNGHOHHHZWCLNQ&xt=urn:md5:D41D8CD98F00B204E9800998ECF8427E')
 
     for uri in uris:
         match = uriref.match(uri)
-        print util.match_groupdict_table(uri, match)
-        print
-
-
-
-
+        try:
+            print util.match_groupdict_table(uri, match)
+            print
+        except Exception, e:
+            print("Exception at %r" % uri)
+            traceback.print_exc()
